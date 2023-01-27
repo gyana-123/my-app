807bf0e07caa7a3f6997c76a41268b84e4d168df
Gyanshree <gyanashree.malakar@moonraft.com>
2023-01-27 17:17:02 +0530

    useRef tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useEffect, useMemo, useState, useRef } from "react";
 function App() {
+  const [users, setUser] = useState([]);
+  const inputValue = useRef();
+  //initialise search value
+  const [searchValue, setSearchValue] = useState("");
+
+  const clickHandler = () => {
+    const value = inputValue.current.value;
+    if (value === "") {
+      alert(" already empty");
+    } else {
+      setSearchValue("");
+    }
+
+  };
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos").then((result) => {
+      result.json().then((resp) => {
+        console.warn("useafft",resp)
+        setUser(resp);
+      });
+    });
+  }, []);
+  //whenever search value gets updated, we will update user list
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> User List </h1>
+      <input
+        type="text"
+        ref={inputValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        value={searchValue}
+        placeholder="Search by name"
+      />{" "}
+      <span>
+        <button onClick={clickHandler}>clear</button>
+      </span>{" "}
+      <br />
+      <br />
+      <table border="1">
+        <tbody>
+          <tr>
+            <td>User Id</td>
+            <td>Title</td>
+            <td>Mobile</td>
+          </tr>
+          {users
+            .filter((item) => {
+              if (searchValue === "") {
+                return item;
+              } else if (
+                item.title.toLowerCase().includes(searchValue.toLowerCase())
+              ) {
+                return item;
+              }
+            })
+            .map((item, i) => (
+              <tr key={i}>
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+                <td>{}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }
-
 export default App;
