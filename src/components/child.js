54e62dc6d35a456185920e48b0d9743acc4f84d0
Gyanshree <gyanashree.malakar@moonraft.com>
2023-01-31 16:46:52 +0530

    darklight mode

---
+++ src/components/child.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div className="main" >
+    {/* //   <p className="sign" align="center">
+    //     Sign in
+    //   </p>
+    //   <form className="form1">
+    //     <input className="username" type="text" placeholder="Username" />
+    //     <input className="password" type="password" placeholder="Password" />
+    //     <a className="submit" align="center">
+    //       Sign in
+    //     </a>
+    //     <p >
+    //       <a href="#">Forgot Password? </a>
+    //     </p>
+    //   </form> */}
+    <div className="forgot" align="center">Child Component</div> </div>
+    // <div  className="main">my child components</div>
+  );
+}
+
+export default Form;
